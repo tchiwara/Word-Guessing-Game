c9d2a4acdfea28c3bfb446c77e004ee57500bab9
tchiwara <tinotendachiwara47@gmail.com>
2026-03-21 13:16:37 +0530

    refactor: convert enum constants to uppercase

--- src/main/java/com/tinotenda/wordGuessGame/Difficulty.java
+++ src/main/java/com/tinotenda/wordGuessGame/Difficulty.java
@@ -2,9 +2,9 @@ package com.tinotenda.wordGuessGame;
 
 enum Difficulty{
 
-    Easy(10,new String[]{"cat", "dog", "run", "jump", "code"}),
-    Medium(5,new String[]{"python", "syntax", "method", "interface", "boolean"}),
-    Hard(3,new String[]{"inheritance", "polymorphism", "abstraction", "encapsulation","recursion"});
+    EASY(10,new String[]{"cat", "dog", "run", "jump", "code"}),
+    MEDIUM(5,new String[]{"python", "syntax", "method", "interface", "boolean"}),
+    HARD(3,new String[]{"inheritance", "polymorphism", "abstraction", "encapsulation","recursion"});
 
     final private int attempts;
     final private String[] wordDifficulty;
@@ -15,7 +15,7 @@ enum Difficulty{
     }
 
     public int getAttempts(){
-        return this.attempts;//why is the keyword unnecessary here?..because there is no naming conflict
+        return this.attempts;//why is the keyword unnecessary here?..because there is no naming conflict here
     }
 
     public String[] getWordDifficulty(){
